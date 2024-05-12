8e27213f73e3b134df17912ef009a26f0c8b204b
Patrick Kabwe <patrickckabwe@gmail.com>
2024-05-12 14:23:10 +0200

    test: errorMap

---
+++ errorMap_test.go
@@ -0,0 +1,39 @@
+package validator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestErrorMap(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    errorMap
+		expected map[string]string
+	}{
+		{
+			name:     "empty error map",
+			input:    errorMap{},
+			expected: map[string]string{},
+		},
+		{
+			name: "error map with one error",
+			input: errorMap{"email": ErrEmailNotValid },
+			expected: map[string]string{"email": ErrEmailNotValid.Error()},
+		},
+		{
+			name: "error map with multiple errors",
+			input: errorMap{"email": ErrEmailNotValid, "phone": ErrNotEmptyField},
+			expected: map[string]string{"email": ErrEmailNotValid.Error(), "phone": ErrNotEmptyField.Error()},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.input.Error(), tc.expected)
+		})
+	}
+}
+
